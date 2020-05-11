fdf5f6d64dc30cddbb45e423b89ce4aa9e3ff5cb
Xirali Sdet <khanyolwas@gmail.com>
2020-05-11 12:01:04 -0400

    my second commit

--- src/main/java/com/testngframework/loginAppilication.java
+++ src/main/java/com/testngframework/loginAppilication.java
@@ -1,5 +1,21 @@
 package com.testngframework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class loginAppilication {
+	@Test
+	public void login()
+	
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		LoginPage login=new LoginPage();
+		
+		
+		
+	}
 
 }
