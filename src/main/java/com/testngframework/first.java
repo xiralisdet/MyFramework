056b6ed6fad2f4388f0b055d84dfa08eb3f12443
Xirali Sdet <khanyolwas@gmail.com>
2020-05-11 11:41:26 -0400

    my initial commit

---
+++ src/main/java/com/testngframework/first.java
@@ -0,0 +1,17 @@
+package com.testngframework;
+
+import org.testng.annotations.Test;
+
+public class first {
+	@Test
+	public void demo()
+	{
+		System.out.println("hello world");
+	}
+	@Test
+	public void second()
+	{
+		System.out.println("hello people");
+	}
+
+}
---
+++ src/main/java/com/testngframework/loginAppilication.java
@@ -0,0 +1,5 @@
+package com.testngframework;
+
+public class loginAppilication {
+
+}
